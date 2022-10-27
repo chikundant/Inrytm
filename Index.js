810f932ce0d8b793922d636c1e849944f91123f8
Chikunda <chikunda.dima@gmail.com>
2022-10-27 20:58:45 +0300

    add tet

--- Index.js
+++ Index.js
@@ -11,4 +11,22 @@ window.onload=function(){
   question[2].addEventListener('click', function(e){
     answer[2].classList.toggle('hide')
   })
+  question[3].addEventListener('click', function(e){
+    answer[3].classList.toggle('hide')
+  })
+  question[4].addEventListener('click', function(e){
+    answer[4].classList.toggle('hide')
+  })
+  question[5].addEventListener('click', function(e){
+    answer[5].classList.toggle('hide')
+  })
+  question[6].addEventListener('click', function(e){
+    answer[6].classList.toggle('hide')
+  })
+  question[7].addEventListener('click', function(e){
+    answer[7].classList.toggle('hide')
+  })
+  question[8].addEventListener('click', function(e){
+    answer[8].classList.toggle('hide')
+  })
 }
