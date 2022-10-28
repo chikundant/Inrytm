582642ce20ba544f9bedc4d30f5f900b9027241c
Chikunda <chikunda.dima@gmail.com>
2022-10-28 03:06:36 +0300

    change some styles

--- Index.js
+++ Index.js
@@ -1,32 +1,31 @@
-window.onload=function(){
-  const question = document.getElementsByClassName('question');
-  const answer = document.getElementsByClassName("answer");
 
-  question[0].addEventListener('click', function(e){
-    answer[0].classList.toggle('hide')
-  })
-  question[1].addEventListener('click', function(e){
-    answer[1].classList.toggle('hide')
-  })
-  question[2].addEventListener('click', function(e){
-    answer[2].classList.toggle('hide')
-  })
-  question[3].addEventListener('click', function(e){
-    answer[3].classList.toggle('hide')
-  })
-  question[4].addEventListener('click', function(e){
-    answer[4].classList.toggle('hide')
-  })
-  question[5].addEventListener('click', function(e){
-    answer[5].classList.toggle('hide')
-  })
-  question[6].addEventListener('click', function(e){
-    answer[6].classList.toggle('hide')
-  })
-  question[7].addEventListener('click', function(e){
-    answer[7].classList.toggle('hide')
-  })
-  question[8].addEventListener('click', function(e){
-    answer[8].classList.toggle('hide')
-  })
-}
+const question = document.getElementsByClassName('question');
+const answer = document.getElementsByClassName("answer");
+
+question[0].addEventListener('click', function (e) {
+  answer[0].classList.toggle('hide')
+})
+question[1].addEventListener('click', function (e) {
+  answer[1].classList.toggle('hide')
+})
+question[2].addEventListener('click', function (e) {
+  answer[2].classList.toggle('hide')
+})
+question[3].addEventListener('click', function (e) {
+  answer[3].classList.toggle('hide')
+})
+question[4].addEventListener('click', function (e) {
+  answer[4].classList.toggle('hide')
+})
+question[5].addEventListener('click', function (e) {
+  answer[5].classList.toggle('hide')
+})
+question[6].addEventListener('click', function (e) {
+  answer[6].classList.toggle('hide')
+})
+question[7].addEventListener('click', function (e) {
+  answer[7].classList.toggle('hide')
+})
+question[8].addEventListener('click', function (e) {
+  answer[8].classList.toggle('hide')
+})
